253e7808c607f2d7590babd9faef95dd7d3ab903
tobiolabode <tobi@tobiolabode.com>
2024-06-27 13:02:12 +0100

    add print statements for warp_color debugging

--- models/FrameColor_stego.py
+++ models/FrameColor_stego.py
@@ -8,7 +8,11 @@ def warp_color(IA_l, IB_lab,
     features_B, vggnet, nonlocal_net, feature_noise=0, temperature=0.01):
     print('calling wrap_color')
     IA_rgb_from_gray = gray2rgb_batch(IA_l)
+    print('nonlocal_net: ', nonlocal_net)
     print('IA_rgb_from_gray: ', IA_rgb_from_gray.shape)
+    print('cluster_value_current: ', cluster_value_current.shape)
+    print('cluster_value_ref: ', cluster_value_ref.shape)
+    print('features_B: ', cluster_value_ref.shape)
     with torch.no_grad():
         ablation_time = time.time()
         A_relu1_1, A_relu2_1, A_relu3_1, A_relu4_1, A_relu5_1 = vggnet(
@@ -31,7 +35,16 @@ def warp_color(IA_l, IB_lab,
     B_relu5_1 = feature_normalize(B_relu5_1)
 
     ablation_time = time.time()
+    
     # operations break around here
+    # Error occurred in epoch 0, iteration 0: Given groups=1, weight of size [256, 256, 1, 1], expected input[1, 1, 4, 256] to have 256 channels, but got 1 channels instead
+    # B_lab_map: [batch_size, 3, height, width]
+    # cluster_value_current and cluster_value_ref: [batch_size, 16, 16]
+    # cluster_preds_current and cluster_preds_ref: [batch_size, 16, 16]
+    # A_relu2_1 and B_relu2_1: [batch_size, 128, height/2, width/2]
+    # A_relu3_1 and B_relu3_1: [batch_size, 256, height/4, width/4]
+    # A_relu4_1 and B_relu4_1: [batch_size, 512, height/8, width/8]
+    # A_relu5_1 and B_relu5_1: [batch_size, 512, height/16, width/16]
     out_tensor_warp = nonlocal_net(
         IB_lab,
         cluster_value_current,
